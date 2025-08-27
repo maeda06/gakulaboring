be42ccd4f911f8826a5075b755f540c4749e4170
maeda <maeda.ryusei@chabdra-inc.com>
2025-08-27 22:27:28 +0900

    ベースコーディングPC

---
+++ js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".faq-content").forEach(function (el) {
+    const summary = el.querySelector(".faq-q");
+    const answer = el.querySelector(".faq-a");
+    summary.addEventListener("click", (event) => {
+      // デフォルトの挙動を無効化
+      event.preventDefault();
+      // detailsのopen属性を判定
+      if (el.getAttribute("open") !== null) {
+        // アコーディオンを閉じるときの処理
+        const closingAnim = answer.animate(closingAnimation(answer), animTiming);
+
+        closingAnim.onfinish = () => {
+          // アニメーションの完了後にopen属性を取り除く
+          el.removeAttribute("open");
+        };
+      } else {
+        // open属性を付与
+        el.setAttribute("open", "true");
+        // アコーディオンを開くときの処理
+        const openingAnim = answer.animate(openingAnimation(answer), animTiming);
+      }
+    });
+  });
+});
+
+// アニメーションの時間とイージング
+const animTiming = {
+  duration: 300,
+  easing: "ease-in-out",
+};
