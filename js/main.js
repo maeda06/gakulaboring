623a5d51974bff1787841884ee5d13c2b5ca68a6
maeda <maeda.ryusei@chabdra-inc.com>
2025-08-31 22:54:55 +0900

    アニメーション追加

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".faq-content").forEach(function (el) {
-    const summary = el.querySelector(".faq-q");
-    const answer = el.querySelector(".faq-a");
-    summary.addEventListener("click", (event) => {
-      // デフォルトの挙動を無効化
-      event.preventDefault();
-      // detailsのopen属性を判定
-      if (el.getAttribute("open") !== null) {
-        // アコーディオンを閉じるときの処理
-        const closingAnim = answer.animate(closingAnimation(answer), animTiming);
+  $(window).on("load", function() {
+    $('body').fadeIn(2000);
+    window.setTimeout(function(){
+    $(".asobuyouni").addClass("is-show");
+    $(".sigotowosuru").addClass("is-show");
+  }, 1500);
 
-        closingAnim.onfinish = () => {
-          // アニメーションの完了後にopen属性を取り除く
-          el.removeAttribute("open");
-        };
-      } else {
-        // open属性を付与
-        el.setAttribute("open", "true");
-        // アコーディオンを開くときの処理
-        const openingAnim = answer.animate(openingAnimation(answer), animTiming);
-      }
-    });
+  window.setTimeout(function(){
+    $("header").addClass("is-show");
+    $("#kv > .cta").addClass("is-show");
+  }, 4000);
+});
+
+  $(function(){
+  $(".inview").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
   });
+});
+
+  // document.querySelectorAll(".faq-content").forEach(function (el) {
+  //   const summary = el.querySelector(".faq-q");
+  //   const answer = el.querySelector(".faq-a");
+  //   summary.addEventListener("click", (event) => {
+  //     // デフォルトの挙動を無効化
+  //     event.preventDefault();
+  //     // detailsのopen属性を判定
+  //     if (el.getAttribute("open") !== null) {
+  //       // アコーディオンを閉じるときの処理
+  //       const closingAnim = answer.animate(closingAnimation(answer), animTiming);
+
+  //       closingAnim.onfinish = () => {
+  //         // アニメーションの完了後にopen属性を取り除く
+  //         el.removeAttribute("open");
+  //       };
+  //     } else {
+  //       // open属性を付与
+  //       el.setAttribute("open", "true");
+  //       // アコーディオンを開くときの処理
+  //       const openingAnim = answer.animate(openingAnimation(answer), animTiming);
+  //     }
+  //   });
+  // });
 });
 
 // アニメーションの時間とイージング
