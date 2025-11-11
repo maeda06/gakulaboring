a0b5033f0f638b2bc35120aabe9bc507c1db6e61
maeda <maeda.ryusei@chandra-inc.com>
2025-11-11 18:35:17 +0900

    cssの出し分けをfunctions.phpに追加

--- functions.php
+++ functions.php
@@ -13,3 +13,19 @@ add_filter( 'excerpt_length', function( $length ){
 add_filter( 'excerpt_more', function( $more ){
   return '…';
 }, 999 );
+
+/*----------------------------------------------------------*/
+/* テンプレートファイルごとにcssを出しわける */
+/*----------------------------------------------------------*/
+function my_styles() {
+    if ( is_front_page() ) {
+        wp_enqueue_style( 'front-page', get_template_directory_uri() . '/css/front-page.css', array() );
+    }elseif( is_page('portfolio') ) {
+      wp_enqueue_style( 'portfolio', get_template_directory_uri() . '/css/page-portfolio.css', array() );
+    }elseif( is_page('review') ) {
+      wp_enqueue_style( 'review', get_template_directory_uri() . '/css/page-review.css', array() );
+    }elseif( is_page('chat') ) {
+      wp_enqueue_style( 'chat', get_template_directory_uri() . '/css/page-chat.css', array() );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'my_styles' );
