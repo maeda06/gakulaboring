b879eb13190ebd5823dfca821a71b03c151ebd3c
maeda-mac <maeda@maeda-ryuseinoMacBook-Air.local>
2025-12-18 15:33:40 +0900

    みんなでスキルアップページ（ベースコーディング）

--- functions.php
+++ functions.php
@@ -28,6 +28,8 @@ function my_styles() {
       wp_enqueue_style( 'review', get_template_directory_uri() . '/css/page-review.css', array() );
     }elseif( is_page('chat') ) {
       wp_enqueue_style( 'chat', get_template_directory_uri() . '/css/page-chat.css', array() );
+    }elseif( is_page('skillup') ) {
+      wp_enqueue_style( 'skillup', get_template_directory_uri() . '/css/page-skillup.css', array() );
     }elseif( is_page('contact-company') ) {
       wp_enqueue_style( 'contact-company', get_template_directory_uri() . '/css/contact-company.css', array() );
     }elseif( is_page('contact-worker') ) {
---
+++ page-skillup.php
@@ -0,0 +1,80 @@
+<?php get_header(); ?>
+<main>
+	<section id="hero">
+		<h1>見れば見るほど</h1>
+		<h1>じぶんが好きになる</h1>
+		<p class="sub">知らなかった<span>可能性</span>、ここにある</p>
+		<div class="kv__cta">
+      <p class="kv__cta-text">無料登録して、動画講座見てみてみよ！</p>
+      <a href="#" class="kv__button">動画でスキルアップ</a>
+    </div>
+	</section>
+	<section id="what">
+		<h2>何ができる</h2>
+		<ul>
+			<li>初心者向けから上級まで、<span>いろんな動画講座が見放題</span></li>
+			<li><span>「Figma」「Canva」「動画編集」「SNSマーケ」「お金の勉強」</span></li>
+			<li>現場で役立つ知識だけギュッと凝縮！<span>10分からサクッと学べるから続けやすい◎</span></li>
+		</ul>
+	</section>
+	<section id="point">
+		<img class="tako" src="<?php echo get_template_directory_uri() ?>/images/tako.png" alt="">
+		<h2 class="section-title section-title--white">嬉しいポイント</h2>
+		<ul>
+			<li>ポートフォリオ、どう作るのが正解？</li>
+			<li>副業やってみたいけど、スキルが…</li>
+			<li>ITとか苦手だけど、少しは知っときたい</li>
+			<li>なんか、周りと差をつけたいかも…！</li>
+			<li>講座ってガチすぎて怖い…もっと気軽に学びたい</li>
+		</ul>
+	</section>
+	<section id="flow">
+		<div class="flow__inner">
+        <h2 class="section-title section-title--white">ご利用の流れ</h2>
+        <div class="flow__list">
+          <div class="flow-item">
+            <span class="flow-item__number">1</span>
+            <div class="flow-item__content">
+              <p>学生・フリーランスとして登録（無料）</p>
+            </div>
+          </div>
+          <div class="flow-item">
+            <span class="flow-item__number">2</span>
+            <div class="flow-item__content">
+              <p>「しゃべり場」にログインして、タイムラインをチェック</p>
+            </div>
+          </div>
+          <div class="flow-item">
+            <span class="flow-item__number">3</span>
+            <div class="flow-item__content">
+              <p>「これ気になる」<br>「わかる！」と思ったら反応してみよう</p>
+            </div>
+          </div>
+          <div class="flow-item">
+            <span class="flow-item__number">4</span>
+            <div class="flow-item__content">
+              <p>自分の悩みや就活エピソードも気軽に投稿！</p>
+            </div>
+          </div>
+        </div>
+      </div>
+	</section>
+	<section id="course">
+		<div class="course__inner">
+        <h2 class="section-title section-title--black">ご利用にはログインが必要です</h2>
+        <p>※講座は順次追加予定！「今欲しい！」が見つかるかも。</p>
+        <div class="course__content">
+          <ul>
+            <li><img src="" alt=""><p>デザインFigma/Canva/Illustrator/Photoshop/STUDIO</p></li>
+            <li><img src="" alt=""><p>動画編集（初級〜上級）</p></li>
+            <li><img src="" alt="">SNSマーケLINE公式運用/Lステップ運用</li>
+            <li><img src="" alt="">ECサイト制作（Shopify）ホームページ制作（WordPress）</li>
+            <li><img src="" alt="">ライティングマーケティング／お金の勉強</li>
+            <li><img src="" alt="">ITパスポート対策 などなど！</li>
+          </ul>
+        </div>
+        <a href="#" class="login-cta__button"><img src="<?php echo get_template_directory_uri(); ?>/images/cta-btn--sub.png" alt=""></a>
+      </div>
+	</section>
+</main>
+<?php get_footer(); ?>
